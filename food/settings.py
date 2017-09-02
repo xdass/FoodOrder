bf6a722f0f8c799a1e0daea6f3e729e4ecce8ab9
dmitry <420600@gmail.com>
2017-09-02 23:08:07 +0500

    correct MEDIA_URL

--- food/settings.py
+++ food/settings.py
@@ -143,7 +143,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_URL = '%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTHENTICATION_BACKENDS = (
    'social_core.backends.vk.VKOAuth2',
